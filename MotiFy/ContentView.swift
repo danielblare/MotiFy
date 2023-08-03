c003f9076c31ea602599c06c63d8c10e1fe79733
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2023-08-03 21:29:39 +0100

    Initial Commit

---
+++ MotiFy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotiFy
+//
+//  Created by Daniel on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotiFy/MotiFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiFyApp.swift
+//  MotiFy
+//
+//  Created by Daniel on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MotiFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
