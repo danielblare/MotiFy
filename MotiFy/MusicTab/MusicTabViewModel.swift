9d2c7400adc2eb928d952714582ea58f7da75dd9
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2023-08-09 17:29:04 +0100

    Fetching algorithm

--- MotiFy/MusicTab/MusicTabViewModel.swift
+++ MotiFy/MusicTab/MusicTabViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import AVKit
 
-struct Track: Codable, Identifiable {
+struct Track: Codable, Identifiable, Equatable {
     let id: String
     let title: String
     let genre: String
@@ -38,6 +38,8 @@ struct Track: Codable, Identifiable {
     }
     
     static let testInstance = Track(id: "yKjoNS0o5YkgFSIADjPF", title: "Test Title", genre: "Test genge", audio: URL(string: "https://firebasestorage.googleapis.com:443/v0/b/motify-f7252.appspot.com/o/MorningMudd%2Faudio.mp3?alt=media&token=88b7a262-7df0-4c9e-b435-cea18a6f22ec")!, artwork: URL(string: "https://firebasestorage.googleapis.com:443/v0/b/motify-f7252.appspot.com/o/MorningMudd%2Fartwork.png?alt=media&token=654a8ea0-5a02-48e7-924c-083a48b48918")!, description: "Test description", duration: CMTime(seconds: 60, preferredTimescale: 600))
+    
+    static let offlineInstance = Track(id: "DHA4WlsFJMJCfCK1DTu3", title: "Title", genre: "Genre", audio: URL(string: "https://youtu.be/T_ASKLftsLs")!, artwork: URL(string: "https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png")!, description: "Description", duration: CMTime(seconds: 60 * 59, preferredTimescale: 600))
 }
 
 @MainActor
@@ -51,7 +53,7 @@ final class MusicTabViewModel: ObservableObject {
         self.tracks.first(where: { $0.id == trackPlayingID })
     }
     
-    @Published private(set) var tracks: [Track] = [.init(id: "DHA4WlsFJMJCfCK1DTu3", title: "Title", genre: "Genre", audio: URL(string: "https://youtu.be/T_ASKLftsLs")!, artwork: URL(string: "https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png")!, description: "Description", duration: CMTime(seconds: 60 * 59, preferredTimescale: 600))]
+    @Published private(set) var tracks: [Track] = [.offlineInstance]
     
     @Published private(set) var isPlaying: Bool = false
     @Published private(set) var currentTime: CMTime = .zero
@@ -65,27 +67,29 @@ final class MusicTabViewModel: ObservableObject {
             }
         }
         
-//        if let data = UserDefaults.standard.data(forKey: "tracks"),
-//           let tracks = try? JSONDecoder().decode([Track].self, from: data) {
-//            self.tracks = tracks
-//        }
-//        Task {
-//            if let trackModels = try? await dependencies.firestoreManager.getTracks() {
-//
-//                var tracks: [Track] = []
-//                
-//                for model in trackModels {
-//                    if let track = try? await Track(from: model, storageManager: dependencies.storageManager) {
-//                        tracks.append(track)
-//                    }
-//                }
-//                self.tracks = tracks
-//                
-//                if let data = try? JSONEncoder().encode(tracks) {
-//                    UserDefaults.standard.setValue(data, forKey: "tracks")
-//                }
-//            }
-//        }
+        if let data = UserDefaults.standard.data(forKey: "tracks"),
+           let tracks = try? JSONDecoder().decode([Track].self, from: data) {
+            self.tracks = tracks
+        }
+        Task {
+            if let trackModels = try? await dependencies.firestoreManager.getTracks() {
+
+                var tracks: [Track] = []
+                
+                for model in trackModels {
+                    if let track = try? await Track(from: model, storageManager: dependencies.storageManager) {
+                        tracks.append(track)
+                    }
+                }
+                if tracks != self.tracks, !tracks.isEmpty {
+                    self.tracks = tracks
+                    
+                    if let data = try? JSONEncoder().encode(tracks) {
+                        UserDefaults.standard.setValue(data, forKey: "tracks")
+                    }
+                }
+            }
+        }
     }
         
     func play(_ track: Track) {
